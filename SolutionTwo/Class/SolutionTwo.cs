ef6cf320a89736889976743143c5291acbbb4182
Jman87 <juanjomartinezv2707@gmail.com>
2026-02-15 20:37:15 -0500

    Revert "Unecessary files"
    This reverts commit a31b4cc315f280f2a6e018329243d83eb2bd8ba3.

---
+++ SolutionTwo/Class/SolutionTwo.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SolutionTwo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    
+    
+    
+    
+    
+    void Start()
+    {
+        Character NewCharacter = new Character();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SolutionTwo/Class/Wizard.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Wizard : Character
+{
+    public int dice_type = 6;
+    public int average_roll = 4;
+}
