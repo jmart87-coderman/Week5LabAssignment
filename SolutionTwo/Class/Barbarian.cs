aa7c2e40e313db85aa82d32dcfa68b2d0cd87855
Jman87 <juanjomartinezv2707@gmail.com>
2026-02-15 20:47:20 -0500

    Removed duplicates

--- SolutionTwo/Class/Barbarian.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class Barbarian : Character
-{
-    public int dice_type = 8;
-    public int average_roll = 5;
-}
---
+++ SolutionTwo/SolutionTwo/Race.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Race
+{
+    public string name {get;}
+    public int HPBonus {get;}
+
+
+    public Race (string name, int HPBonus)
+    {
+        this.name = name;
+        this.HPBonus = HPBonus;
+    }
+    
+}
+
+
+
